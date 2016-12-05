5b3ff4b7fdaa55d5d2b39a38d9e96b99a5d41319
dannysem <dannysem@st.technion.ac.il@ml6.iem.technion.ac.il>
2016-12-05 13:41:15 +0200

    restored batch size + train data normalizing

--- EX1 - MNIST DB Classifier/classification_mnist_load_model.lua
+++ EX1 - MNIST DB Classifier/classification_mnist_load_model.lua
@@ -15,14 +15,17 @@ local function forwardNet(data, labels)
     --another helpful function of optim is ConfusionMatrix
     local confusion = optim.ConfusionMatrix(torch.range(0,9):totable())
     local lossAcc = 0
+	local numBatches = 0
 
-	-- predicting
-	local x = data:cuda()
-	local yt = labels:cuda()
-	local y = model:forward(x)
-	local err = criterion:forward(y, yt)
-	lossAcc = lossAcc + err
-	confusion:batchAdd(y,yt)
+    for i = 1, data:size(1) - batchSize, batchSize do
+        numBatches = numBatches + 1
+        local x = data:narrow(1, i, batchSize):cuda()
+        local yt = labels:narrow(1, i, batchSize):cuda()
+        local y = model:forward(x)
+        local err = criterion:forward(y, yt)
+        lossAcc = lossAcc + err
+        confusion:batchAdd(y,yt)
+    end
     
     confusion:updateValids()
     local avgError = 1 - confusion.totalValid
@@ -34,11 +37,13 @@ function load_model()
 	-- load the model
 	model = torch.load('network.model')
 	
+	local trainData = mnist.traindataset().data:float();
+	local trainLabels = mnist.traindataset().label:add(1);
 	testLabels = mnist.testdataset().label:add(1);
 	testData = mnist.testdataset().data:float();
 	--We'll start by normalizing our data
-	local mean = testData:mean()
-	local std = testData:std()
+	local mean = trainData:mean()
+	local std = trainData:std()
 	testData:add(-mean):div(std);
 	
 	-- run on test set
