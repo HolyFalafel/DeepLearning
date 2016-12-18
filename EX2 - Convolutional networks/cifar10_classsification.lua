b14305d491323f1b29a624e620e103c71384e768
dannysem <dannysem@st.technion.ac.il@ml6.iem.technion.ac.il>
2016-12-18 21:16:48 +0200

    fully connected param reduce + lobal correct: 70.873 with just a little overfitting

--- EX2 - Convolutional networks/cifar10_classsification.lua
+++ EX2 - Convolutional networks/cifar10_classsification.lua
@@ -84,20 +84,20 @@ end
 --  ****************************************************************
 
 local model = nn.Sequential()
-model:add(cudnn.SpatialConvolution(3, 32, 5, 5)) -- 3 input image channel, 32 output channels, 5x5 convolution kernel
+model:add(cudnn.SpatialConvolution(3, 64, 5, 5)) -- 3 input image channel, 32 output channels, 5x5 convolution kernel
 model:add(cudnn.ReLU(true))                          -- ReLU activation function
 model:add(cudnn.SpatialMaxPooling(2,2,2,2))      -- A max-pooling operation that looks at 2x2 windows and finds the max.
-model:add(nn.SpatialBatchNormalization(32))    --Batch normalization will provide quicker convergence
-model:add(cudnn.SpatialConvolution(32, 64, 3, 3))
+model:add(nn.SpatialBatchNormalization(64))    --Batch normalization will provide quicker convergence
+model:add(cudnn.SpatialConvolution(64, 16, 3, 3))
 model:add(cudnn.SpatialMaxPooling(2,2,2,2))
 model:add(cudnn.ReLU(true))
-model:add(nn.SpatialBatchNormalization(64))
-model:add(cudnn.SpatialConvolution(64, 8, 3, 3))
+model:add(nn.SpatialBatchNormalization(16))
+model:add(cudnn.SpatialConvolution(16, 8, 3, 3))
 model:add(nn.View(8*4*4):setNumInputDims(3))  -- reshapes from a 3D tensor of 32x4x4 into 1D tensor of 32*4*4
-model:add(nn.Linear(8*4*4, 128))             -- fully connected layer (matrix multiplication between input and weights)
+model:add(nn.Linear(8*4*4, 64))             -- fully connected layer (matrix multiplication between input and weights)
 model:add(cudnn.ReLU(true))
 model:add(nn.Dropout(0.5))                      --Dropout layer with p=0.5
-model:add(nn.Linear(128, #classes))            -- 10 is the number of outputs of the network (in this case, 10 digits)
+model:add(nn.Linear(64, #classes))            -- 10 is the number of outputs of the network (in this case, 10 digits)
 model:add(nn.LogSoftMax())                     -- converts the output to a log-probability. Useful for classificati
 
 model:cuda()
@@ -173,7 +173,7 @@ end
 
 ---------------------------------------------------------------------
 
-epochs = 30 --50
+epochs = 50
 trainLoss = torch.Tensor(epochs)
 testLoss = torch.Tensor(epochs)
 trainError = torch.Tensor(epochs)
